5e359abf536757c044dd7f147c1cab87bb5ea3d1
elbertribeiro <elbertprofissional@gmail.com>
2022-03-05 11:05:19 -0300

    Refatoração do cod

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,9 +8,9 @@ export function Navbar(props) {
         <Navegation className="nav-bar">
             <h2>Elbert Ribeiro</h2>
             <Navlist className="nav-list">
-                <li><a className="nav-link" href="pages/curriculo/">{props.text1}</a></li>
+                <li><a className="nav-link" href="../../view/resume/">{props.text1}</a></li>
                 <li><Link to="/resume" className="nav-link">{props.text2}</Link></li>
-                <li><Link to={props.page} className="nav-link" href="pages/curriculo/">{props.text3}</Link></li>
+                <li><Link to={props.page} className="nav-link" href="../../view/resume/">{props.text3}</Link></li>
             </Navlist>
             <Droplink className="dropdown-link"><a id="dropdownLink" href="./">&#9776;</a></Droplink>
         </Navegation>
